2ab1410dcd2352c621a36eb9d1747ea52e3e048d
mkavalevich <ritavalerievna@gmail.com>
2019-07-22 22:08:53 +0200

    Homework lesson 3 (Task 10,16,19)

---
+++ src/lesson02Tasks/lesson02Task19.java
@@ -0,0 +1,30 @@
+package lesson02Tasks;
+
+import java.util.Scanner;
+
+public class lesson02Task19 {
+    public static void main(String[] args) {
+        int m, n, c, d;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input number of rows of matrix");
+        m = in.nextInt();
+        System.out.println("Input number of columns of matrix");
+        n  = in.nextInt();
+
+        int array1[][] = new int[m][n];
+        int array2[][] = new int[m][n];
+
+        System.out.println("Input elements of first matrix");
+
+        for (  c = 0 ; c < m ; c++ )
+            for ( d = 0 ; d < n ; d++ )
+                array1[c][d] = in.nextInt();
+
+        System.out.println("Input the elements of second matrix");
+
+        for ( c = 0 ; c < m ; c++ )
+            for ( d = 0 ; d < n ; d++ )
+                array2[c][d] = in.nextInt();
+    }
+}
