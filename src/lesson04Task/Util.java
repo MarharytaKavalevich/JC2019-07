4fb227a1294d9e31ed419b7eb0e6b3414ecd0add
MarharytaKavalevich <49939867+MarharytaKavalevich@users.noreply.github.com>
2019-08-01 22:46:56 +0300

    Homework lesson 4

--- src/lesson04Task/Util.java
+++ src/lesson04Task/Util.java
@@ -4,4 +4,4 @@ public class Util {
     public static double carPrice (Car car){
         return car.getDoor().getPrice() + car.getEngine().getPrice() + car.getWheel().getPrice() + car.getBody().getPrice() + car.getChassis().getPrice();
     }
-}
+} 
