8be1d541569fbbabd0cbb558644d53678ebf26fa
MarharytaKavalevich <49939867+MarharytaKavalevich@users.noreply.github.com>
2019-08-01 22:40:14 +0300

    Update Util.java

--- src/lesson04Task/Util.java
+++ src/lesson04Task/Util.java
@@ -4,6 +4,4 @@ public class Util {
     public static double carPrice (Car car){
         return car.getDoor().getPrice() + car.getEngine().getPrice() + car.getWheel().getPrice() + car.getBody().getPrice() + car.getChassis().getPrice();
     }
-    public static String carFirm = "Mercedes";
-    public static String carModel = "CLS";
 }
